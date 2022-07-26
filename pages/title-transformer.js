e2124e9d844f3979c630ba19c39bc8ea67162ec4
giwangdk <gidwikintan@gmail.com>
2022-07-26 16:54:01 +0700

    feat(integration): :sparkles: integrate feature add and remove titlechakra

--- pages/title-transformer.js
+++ pages/title-transformer.js
@@ -7,8 +7,31 @@ import {
   Stack,
   Text,
 } from '@chakra-ui/react';
+import { useState } from 'react';
 
 function TitleTransformer() {
+  const [title, setTitle] = useState([
+    {
+      id: 1,
+      title: 'Intenship Program',
+    },
+  ]);
+
+  const handleAddTitle = () => {
+    setTitle([
+      ...title,
+      {
+        id: title.length + 1,
+        title: 'Intenship Program',
+      },
+    ]);
+  };
+
+  const handleRemoveTitle = () => {
+    if (title.length > 1) {
+      setTitle(title.slice(0, -1));
+    }
+  };
   return (
     <Flex
       justifyContent="center"
@@ -16,15 +39,23 @@ function TitleTransformer() {
       height="100vh"
       alignItems="center"
     >
-      <Heading as="h1" size="xl">
-        Internship Program
-      </Heading>
+      {title.map((item) => (
+        <Heading as="h1" size="xl" my={4}>
+          {item.title}
+          {' '}
+          -
+          {item.id}
+        </Heading>
+      ))}
 
       <Box my={5}>
         <Divider orientation="horizontal" />
         <Stack p={3} spacing={5} direction="row">
           <Text>Running for: 1335s</Text>
-          <Text>Title Count: 1335s</Text>
+          <Text>
+            Title Count:
+            {title.length}
+          </Text>
         </Stack>
         <Divider orientation="horizontal" />
       </Box>
@@ -34,11 +65,18 @@ function TitleTransformer() {
           Clear Color
           {' '}
         </Button>
-        <Button variantColor="gray" display={['block']}>
+        <Button
+          onClick={() => handleAddTitle()}
+          variantColor="gray"
+          display={['block']}
+          disabled={title.length >= 5}
+        >
           Add title
           {' '}
         </Button>
-        <Button variantColor="gray">Remove Title</Button>
+        <Button variantColor="gray" onClick={() => handleRemoveTitle()}>
+          Remove Title
+        </Button>
       </Stack>
     </Flex>
   );
