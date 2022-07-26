6f27a1b6188f8be7b16ff2e35954d3b38370c2fd
giwangdk <gidwikintan@gmail.com>
2022-07-26 16:24:27 +0700

    feat(layouting): :sparkles: layouting title_transformer_page :construction:

--- pages/title-transformer.js
+++ pages/title-transformer.js
@@ -1,5 +1,47 @@
+import {
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Heading,
+  Stack,
+  Text,
+} from '@chakra-ui/react';
+
 function TitleTransformer() {
-  return <div>TitleTransformer</div>;
+  return (
+    <Flex
+      justifyContent="center"
+      flexDirection="column"
+      height="100vh"
+      alignItems="center"
+    >
+      <Heading as="h1" size="xl">
+        Internship Program
+      </Heading>
+
+      <Box my={5}>
+        <Divider orientation="horizontal" />
+        <Stack p={3} spacing={5} direction="row">
+          <Text>Running for: 1335s</Text>
+          <Text>Title Count: 1335s</Text>
+        </Stack>
+        <Divider orientation="horizontal" />
+      </Box>
+
+      <Stack spacing={4} direction={['column', 'row']}>
+        <Button variantColor="gray" display={['block']}>
+          Clear Color
+          {' '}
+        </Button>
+        <Button variantColor="gray" display={['block']}>
+          Add title
+          {' '}
+        </Button>
+        <Button variantColor="gray">Remove Title</Button>
+      </Stack>
+    </Flex>
+  );
 }
 
 export default TitleTransformer;
