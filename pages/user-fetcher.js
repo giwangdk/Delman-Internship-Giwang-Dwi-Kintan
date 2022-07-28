72d33c38236e024dd12091a266318f2f8e8e2078
giwangdk <gidwikintan@gmail.com>
2022-07-28 11:15:47 +0700

    feat(integration): :sparkles: integrate user fetcher to card user

--- pages/user-fetcher.js
+++ pages/user-fetcher.js
@@ -1,9 +1,26 @@
 import {
   Box, Button, Divider, Flex, Heading
 } from '@chakra-ui/react';
+import axios from 'axios';
+import { useState } from 'react';
 import CardSkeleton from '../components/CardSkeleton';
+import CardUser from '../components/CardUser';
 
 function UserFetcher() {
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleFetchUser = async () => {
+    try {
+      setIsLoading(true);
+      const response = await axios.get('https://randomuser.me/api');
+      setUser(response.data.results[0]);
+    } catch (e) {
+      alert(e);
+    }
+    console.log(user);
+    setIsLoading(false);
+  };
   return (
     <Flex
       justifyContent="center"
@@ -12,12 +29,17 @@ function UserFetcher() {
       alignItems="center"
     >
       <Box>
-        <Heading as="h1" size="xl" my={4} textAlign="center">
+        <Heading as="h2" size="xl" my={20} textAlign="center">
           User Fetcher
         </Heading>
-        <CardSkeleton />
-        <Divider orientation="horizontal" my={10} />
-        <Button variantColor="gray" width="100%">
+        {user ? <CardUser user={user} /> : <CardSkeleton />}
+        <Divider orientation="horizontal" my={8} />
+        <Button
+          isLoading={isLoading}
+          variantColor="gray"
+          width="100%"
+          onClick={() => handleFetchUser()}
+        >
           Fetch User
         </Button>
       </Box>
